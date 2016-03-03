fc59a25e9523ea4ce12cd2da882587ae0f6dd565
sajinaziz <sajin.aziz@cicondubai.ae>
2016-03-03 10:46:49 +0400

    [ADD] Project Module Project Parent

---
+++ cicon_project_task/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'CICON Project',
+    'version': '1.1',
+    'author': 'CICON',
+    'category': 'Others',
+    'description': """   """,
+    'website': 'http://www.cicon.ae',
+    'depends': ['project'],
+    'data': ['cicon_project_task_view.xml'],
+    # 'data': 'cicon_project_task_view.xml',
+    'test': [],
+    'installable': True,
+    'active': False,
+}
---
+++ cicon_project_task/cicon_project_task.py
@@ -0,0 +1,9 @@
+from openerp import models, fields, api
+
+
+class CiconProject(models.Model):
+    _inherit = 'project.project'
+
+    parent_id = fields.Many2one('project.project', string="Parent Project")
+
+CiconProject()
