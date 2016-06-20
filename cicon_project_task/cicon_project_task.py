d0990970dde5f4acfce35db84e9d6278fb2da39e
Lali <lalitp04@gmail.com>
2016-06-20 09:40:32 +0400

    [TEST] test

--- cicon_project_task/cicon_project_task.py
+++ cicon_project_task/cicon_project_task.py
@@ -4,6 +4,6 @@ from openerp import models, fields, api
 class CiconProject(models.Model):
     _inherit = 'project.project'
 
-    parent_id = fields.Many2one('project.project', string="Parent Project", help="Parent Project")
+    parent_id = fields.Many2one('project.project', string="Parent Project")
 
 CiconProject()
